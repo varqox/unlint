2e1a25d7da04f6c35bca084be19b043a6b8185a0
Krzysztof Małysa <krzyk240@gmail.com>
2013-06-09 23:29:09 +0200

    update

---
+++ unlint/build/CMakeFiles/unlint.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/unlint.dir/main.o"
+  "unlint.pdb"
+  "unlint"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/unlint.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
